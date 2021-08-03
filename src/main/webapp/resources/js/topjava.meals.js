41f3c7cbda80f92dad1769128567f31a341b62b5
Stadger <78420338+Stadger@users.noreply.github.com>
2021-08-03 22:45:49 +0300

    8 HW 1-4

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,51 @@
+const mealAjaxUrl = "UI/meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+// $(document).ready(function () {
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "asc"
+                ]
+            ]
+        })
+    );
+});
+function filter() {
+    $.get(ctx.ajaxUrl+"filter",$('#filterForm').serialize())
+        .done(function (data) {
+            ctx.datatableApi.clear().rows.add(data).draw()});
+}
+
+function filterReset() {
+    $('#filterForm').find(':input').val("");
+    updateTable();
+}
