56198fbbecd58eb76e988c59b47306d41deda15f
Stadger <78420338+Stadger@users.noreply.github.com>
2021-08-11 23:05:48 +0300

    9 HW 5

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -23,7 +23,12 @@ function updateRow(id) {
     $("#modalTitle").html(i18n["editTitle"]);
     $.get(ctx.ajaxUrl + id, function (data) {
         $.each(data, function (key, value) {
-            form.find("input[name='" + key + "']").val(value);
+            if(key==='dateTime')
+                form.find("input[name='" + key + "']").val(value.substr(0, 16).replace('T', ' '));
+
+            else
+                form.find("input[name='" + key + "']").val(value);
+
         });
         $('#editRow').modal();
     });
--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -17,18 +17,48 @@ function clearFilter() {
     $.get(mealAjaxUrl, updateTableByData);
 }
 
-$('#startDate,#endDate').datetimepicker({
-    timepicker:false,
-    format:'Y-m-d'
+$('#endDate').datetimepicker({
+    timepicker: false,
+    format: 'Y-m-d',
+    onShow: function (ct) {
+        this.setOptions({
+            minDate: $('#startDate').val() ? $('#startDate').val() : false
+        })
+    }
 })
 
-$('#startTime,#endTime').datetimepicker({
-    datepicker:false,
-    format:'H:i'
+$('#startDate').datetimepicker({
+    timepicker: false,
+    format: 'Y-m-d',
+    onShow: function (ct) {
+        this.setOptions({
+            maxDate: $('#endDate').val() ? $('#endDate').val() : false
+        })
+    }
+})
+
+$('#startTime').datetimepicker({
+    datepicker: false,
+    format: 'H:i',
+    onShow: function (ct) {
+        this.setOptions({
+            maxTime: $('#endTime').val() ? $('#endTime').val() : false
+        })
+    }
+})
+
+$('#endTime').datetimepicker({
+    datepicker: false,
+    format: 'H:i',
+    onShow: function (ct) {
+        this.setOptions({
+            minTime: $('#startTime').val() ? $('#startTime').val() : false
+        })
+    }
 })
 
 $('#dateTime').datetimepicker({
-    format:'Y-m-d H:i'
+    format: 'Y-m-d H:i'
 })
 
 $(function () {
@@ -45,7 +75,7 @@ $(function () {
                     "data": "dateTime",
                     "render": function (date, type, row) {
                         if (type === "display") {
-                            return date.replace("T", " ");
+                            return date.substr(0, 16).replace('T', ' ');
                         }
                         return date;
                     }
